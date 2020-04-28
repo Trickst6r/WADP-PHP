270226f3f544056c21b52027151983b478757d12
Penis Lord <37201831+Trickst6r@users.noreply.github.com>
2020-04-28 15:34:18 +0700

    Add files via upload

---
+++ Ass3/Ass3_EditProduct_saveDB.php
@@ -0,0 +1,35 @@
+<?php
+    
+    //Kiểm tra xem form điều chỉnh trang [Ass03_AdminProduct.php]
+    if (isset($_GET["btnOK"]) == FALSE) {
+        header("location:Ass3_AdminProduct.php");
+        exit();
+    }
+    
+    //Lấy connection
+    include_once '../../thuvien/connectDB.php';
+    
+    //Lấy hết dữ liệu của form điều chỉnh để lưu lên databse
+        $id = $_GET["txtID"];
+        $name = $_GET["txtName"];
+        $price = $_GET["txtPrice"];
+        $desc = $_GET["txtDesc"];
+        
+    
+        
+    //Viết lệnh update
+    $sql ="UPDATE tbproducts SET name='$name',price=$price, descriptions ='$desc' WHERE code='$id' ;";
+    
+    //Thi hành lệnh truy vấn từ biến connection
+    $r= mysqli_query($link, $sql);
+    if($r==FALSE){
+        echo "<h3 style='color:blue'>Lỗi sai Điều Chỉnh thông tin Product</h3>";
+        exit();
+    }
+    else{
+    //quay ve lai trang danh sach lop
+    header("location:Ass3_AdminProduct.php");
+    }
+    
+    
+?>
